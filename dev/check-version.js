0602d500b90a1be1753c5983a9727a601880bfa3
Tom Gordon <tom.gordon@squareweave.com.au>
2019-08-14 14:36:45 +1000

    Add check-version script and cleanup empty files

---
+++ dev/check-version.js
@@ -0,0 +1,9 @@
+const semver = require('semver');
+const packageJson = require('../package.json');
+const colors = require('colors/safe');
+const version = packageJson.engines.node;
+
+if (!semver.satisfies(process.version, version)) {
+    console.log(colors.red.underline('Required node version' + version + ' not satisfied with current version ' + process.version));
+    process.exit(1);
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,134 +1,135 @@
 import React, { useState, useEffect, Fragment } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import {Formik, Form, Field} from 'formik'
+import { Formik, Form, Field } from 'formik'
 import CustomField from '../components/formField';
 import Output from '../components/Output';
 import Footer from '../components/footer';
 
 
 const standardValues = {
-	nitrite: 0.24,
-	salt: 2.2,
-	sugar: 0.8
+    nitrite: 0.24,
+    salt: 2.2,
+    sugar: 0.8
 }
 
 const ruhlmanValues = {
-	nitrite: 0.22,
-	salt: 2.5,
-	sugar: 1.41
+    nitrite: 0.22,
+    salt: 2.5,
+    sugar: 1.41
 }
 
-const initialSet = Object.assign(standardValues, {weight: 0})
+const initialSet = Object.assign(standardValues, { weight: 0 })
 
 const IndexPage = () => {
 
-	const [dataSource, setDataSource] = useState('standard');
+    const [dataSource, setDataSource] = useState('standard');
 
-	useEffect(() => {
-		console.log('new dataSource is', dataSource);
-	});
-
-
-	const updateData = ((setValues,source, values) => {
-		console.log('update run', source, values.weight);
-		if (source === 'ruhlman') {
-			setValues(Object.assign(ruhlmanValues, {weight:values.weight}));
-
-		} else if (source === 'standard') {
+    useEffect(() => {
+        console.log('new dataSource is', dataSource);
+    });
 
 
-			setValues(Object.assign(standardValues, {weight:values.weight}));
-		}
+    const updateData = ((setValues, source, values) => {
+        console.log('update run', source, values.weight);
+        if (source === 'ruhlman') {
+            setValues(Object.assign(ruhlmanValues, { weight: values.weight }));
+
+        } else if (source === 'standard') {
 
 
-		setDataSource(source)
-	})
+            setValues(Object.assign(standardValues, { weight: values.weight }));
+        }
 
-	return (
-	<Layout>
-			<SEO title="Bacon Calculator" keywords={[`bacon`, `calculator`, `weight`, `nitrate`]} />
-			<p>Hi, I've set up some sensible defaults</p>
-			<Formik
-				initialValues={initialSet}
-				validate={values => {
-					// let errors = {};
-					// if (!values.email) {
-					//   errors.email = 'Required';
-					// } else if (
-					//   !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-					// ) {
-					//   errors.email = 'Invalid email address';
-					// }
-					// return errors;
-				}}
-				onSubmit={(values, { setSubmitting }) => {
-					setTimeout(() => {
-						console.log(JSON.stringify(values, null, 2));
-						setSubmitting(false);
-					}, 400);
-				}}
-			>
-				{({ isSubmitting, errors, values, setValues }) => {
-					return (
-						<Fragment>
-						<button onClick={() => updateData(setValues, 'standard', values)}>Standard</button>
-						<button onClick={() => updateData(setValues, 'ruhlman', values)}>Ruhlman</button>
-						<button onClick={() => updateData(setValues, 'custom', values)}>Custom (to enable fields)</button>
-						<Form style={{textAlign:'center'}}>
 
-							<Field
-								component={CustomField}
-								name="weight"
-								errors={errors}
-								id="weight"
-								type="number"
-								placeholder="in grams"
-								label="Boneless, Rindless Pork Belly (grams)"
-							/>
+        setDataSource(source)
+    })
 
-							<Field
-								readOnly={dataSource !== 'custom' ? true : false}
-								component={CustomField}
-								name="nitrite"
-								errors={errors}
-								placeholder="percent"
-								id="nitrite"
-								type="number"
-								label="Prague Powder No #1 (%) (6-6.25% Sodium Nitrite)"
-							/>
+    return (
+        <Layout>
+            <SEO title="Bacon Calculator" keywords={[`bacon`, `calculator`, `weight`, `nitrate`]} />
+            <p>Hi, I've set up some sensible defaults</p>
+            <Formik
+                initialValues={initialSet}
+                validate={values => {
+                    // let errors = {};
+                    // if (!values.email) {
+                    //   errors.email = 'Required';
+                    // } else if (
+                    //   !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    // ) {
+                    //   errors.email = 'Invalid email address';
+                    // }
+                    // return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        console.log(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
+                {({ isSubmitting, errors, values, setValues }) => {
+                    return (
+                        <Fragment>
+                            <button onClick={() => updateData(setValues, 'standard', values)}>Standard</button>
+                            <button onClick={() => updateData(setValues, 'ruhlman', values)}>Ruhlman</button>
+                            <button onClick={() => updateData(setValues, 'custom', values)}>Custom (to enable fields)</button>
+                            <Form style={{ textAlign: 'center' }}>
 
-							<Field
-								readOnly={dataSource !== 'custom' ? true : false}
-								component={CustomField}
-								name="salt"
-								errors={errors}
-								placeholder="percent"
-								id="salt"
-								type="number"
-								label="Salt (%)"
-							/>
+                                <Field
+                                    component={CustomField}
+                                    name="weight"
+                                    errors={errors}
+                                    id="weight"
+                                    type="number"
+                                    placeholder="in grams"
+                                    label="Boneless, Rindless Pork Belly (grams)"
+                                />
 
-							<Field
-								readOnly={dataSource !== 'custom' ? true : false}
-								component={CustomField}
-								name="sugar"
-								errors={errors}
-								placeholder="percent"
-								id="sugar"
-								type="number"
-								label="Brown Sugar (%)"
-							/>
+                                <Field
+                                    readOnly={dataSource !== 'custom' ? true : false}
+                                    component={CustomField}
+                                    name="nitrite"
+                                    errors={errors}
+                                    placeholder="percent"
+                                    id="nitrite"
+                                    type="number"
+                                    label="Prague Powder No #1 (%) (6-6.25% Sodium Nitrite)"
+                                />
 
-							<Output values={values} />
+                                <Field
+                                    readOnly={dataSource !== 'custom' ? true : false}
+                                    component={CustomField}
+                                    name="salt"
+                                    errors={errors}
+                                    placeholder="percent"
+                                    id="salt"
+                                    type="number"
+                                    label="Salt (%)"
+                                />
 
-						</Form>
-					</Fragment>
-				)}}
-			</Formik>
-			<Footer><a href="//tomgordon.me" target="_blank"><span role="img" aria-label="pig emoji">🐷</span> tom made this <span role="img" aria-label="pig emoji">🐷</span></a></Footer>
-		</Layout>
-	)
+                                <Field
+                                    readOnly={dataSource !== 'custom' ? true : false}
+                                    component={CustomField}
+                                    name="sugar"
+                                    errors={errors}
+                                    placeholder="percent"
+                                    id="sugar"
+                                    type="number"
+                                    label="Brown Sugar (%)"
+                                />
+
+                                <Output values={values} />
+
+                            </Form>
+                        </Fragment>
+                    )
+                }}
+            </Formik>
+            <Footer><a href="//tomgordon.me" rel="noopener noreferrer" target="_blank"><span role="img" aria-label="pig emoji">🐷</span> tom made this <span role="img" aria-label="pig emoji">🐷</span></a></Footer>
+        </Layout>
+    )
 }
 
 
